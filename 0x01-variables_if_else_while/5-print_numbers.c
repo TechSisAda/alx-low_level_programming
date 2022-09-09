f5f8cd125d71f2eab4a19c4bae64db0cab8f6803
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2022-09-09 14:40:52 +0100

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int ate;
+for (ate = 0; ate <= 9; ate++)
+{
+printf("%d", ate);
+}
+printf("\n");
+return (0);
+}
