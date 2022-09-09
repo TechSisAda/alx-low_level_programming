5fab880e3ce9604df60012b7bb0487764c4310c8
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2022-09-09 15:11:45 +0100

    basesixteen

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int ate;
+for (ate = 48; ate <= 57; ate++)
+{
+putchar(ate);
+}
+for (ate = 97; ate <= 102; ate++)
+{
+putchar(ate)
+}
+putchar('\n');
+return (0);
+}
