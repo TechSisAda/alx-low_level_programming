9edcbe4253671a290d15801eaeb8e12d33ba64f5
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2022-09-09 14:53:48 +0100

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int ate;
+for (ate = 48; ate <= 57; ate++)
+{
+putchar(ate);
+}
+putchar('\n');
+return (0);
+}
