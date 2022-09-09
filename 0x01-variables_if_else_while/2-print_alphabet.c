3e81633a3db5ecd9d454784965c7a180fd0a2fb7
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2022-09-09 13:58:26 +0100

    variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int at;
+for (ate = 'a'; ate<= 'z'; ate++)
+{
+putchar(ate);
+}
+putchar('\n');
+return (0);
+}
