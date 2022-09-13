d3a4fb37dc3e6578686189e392b76b27fef34af0
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2022-09-13 20:58:11 +0100

    alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_isalpha - checks for an alphabetic character
+*@c: is an integer arguement
+* Return: Always 0
+*/
+int _isalpha(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else if (c >= 97 && c <= 122)
+{
+return(1);
+}
+return (0);
+}
