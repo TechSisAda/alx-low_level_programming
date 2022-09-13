0b39cf76764f5134b4078ccf1d50f1568f27021f
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2022-09-13 20:28:10 +0100

    lowercase alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
- * print_alphabet - Check description
- * Description: It prints the alphabet in lowercase followed by a new line
- * Return: Nothing.
- */
+* print_alphabet - Check description
+* Description: It prints the alphabet in lowercase followed by a new line
+* Return: Nothing.
+*/
 void print_alphabet(void)
 {
 char low;
 for (low = 'a'; low <= 'z'; low++)
+{
 _putchar(low);
+}
 _putchar('\n');
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_alphabet_x10 
- * Description: function uses _putchar function
- * to print alphabet in lowercase 10 times
- * Return: Nothing.
- */
+* print_alphabet_x10 
+* Description: function uses _putchar function
+* to print alphabet in lowercase 10 times
+* Return: Nothing.
+*/
 void print_alphabet_x10(void)
 {
 int s = 0;
