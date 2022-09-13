ca11356977ba205f844bc789bb106d46f32e775b
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2022-09-13 13:25:14 +0100

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main (void)
+{
+    printf("putchar\n");
+    return 0;
+}
---
+++ 0x02-functions_nested_loops/1-alphabeth.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/*
+* func prints alphabet in lower case x10
+* Return 0
+*/
+void print_alphabet_x10(void)
+{
+int s = 0;
+while (s < 10)
+{
+char t = 'a';
+while (t <= 'z')
+{
+_putchar(t);
+t++;
+}
+_putchar(\n);
+s++;
+}
+}
