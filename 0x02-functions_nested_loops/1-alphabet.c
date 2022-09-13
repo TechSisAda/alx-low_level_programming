d6769c25ac7f5be6ae21a5f3486e9da3fb2fcc43
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2022-09-13 20:12:58 +0100

    lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
-* print_alphabet: It prints the alphabet in lowercase followed next by a new line
-* Return always 0
+* print_alphabet
+* prints lowercase
+* Return: Always 0.
 */
 void print_alphabet(void)
 {
