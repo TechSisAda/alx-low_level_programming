55dea50c8a8b7a4ad37c04aba18a340b15c9f743
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2022-09-13 22:30:48 +0100

    numbers

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print-last_digit: prints last digits of a number
+*@a: the input integer
+* Return: value of last digit of number a.
+*/
+int print_last_digit(int a)
+{
+int n;
+if (a < 0)
+n = -1 * (a % 10);
+else
+n = a % 10;
+_putchar((n % 10) + '0');
+return (n % 10);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+* void times_table - prints 9 times table
+* Description: function prints the 9 times table from 0
+* Return: always 0.
+*/
+void times_table(void)
+{
+int a, b, product;
+for (b = 0; b < 10; b++)
+{
+for (a = 0; a < 10; a++)
+{
+product = a * b;
+if (a == 0)
+{
+_putchar(product + '0');
+}
+else if (product >= 10)
+{
+_putchar(' ');
+_putchar(product / 10 + '0');
+_putchar(product % 10 + '0');
+}
+else
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(product + '0');
+}
+if (a != 9)
+{
+_putchar(',');
+}
+}
+_putchar('\n');
+}
+}
