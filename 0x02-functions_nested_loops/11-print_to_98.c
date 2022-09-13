8f84cb865cbc048440e40aefa696ee3bdf17d3f3
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2022-09-13 23:21:02 +0100

    algebra

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,10 +4,9 @@
 * print_to_98 - prints all natural numbers to 98
 * Description: function prints all natural numbers from n to 98
 *@n: starting integer
-*@a: input integer
 * Return: always 0.
 */
-void print_to_98(int n);
+void print_to_98(int n)
 {
 int a = 0;
 if (n > 98)
