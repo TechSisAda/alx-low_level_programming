196ffde3f4b9376847eec112e3de53d840545edb
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2022-09-13 20:32:28 +0100

    lower case alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
 * print_alphabet_x10 
-* Description: function uses _putchar function
-* to print alphabet in lowercase 10 times
+* Description: function uses _putchar function to print 
+* alphabet in lowercase 10 times
 * Return: Nothing.
 */
 void print_alphabet_x10(void)
