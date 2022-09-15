f2b1019a851713bde745761203f44022dadee98d
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2022-09-15 02:06:32 +0100

    debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
 * largest_number - returns the largest of 3 numbers
+* Description: function prints the largest 3 numbers
 * @a: first integer
 * @b: second integer
 * @c: third integer
@@ -19,8 +20,7 @@ largest = b;
 }
 else if (c >= a && b >= c)
 {
-    largest = c;
+largest = c;
 }
-
 return (largest);
 }
