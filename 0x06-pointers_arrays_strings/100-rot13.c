8c6649d98fba54ee5dd76d81385a1dcd3ca538dd
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2022-09-22 13:18:58 +0100

    More Arrays, Strings, Pointers

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * rot13 -  a function that encodes a string using rot13.
- * @s: An input string to encode using rot13
- * Return: An encode string
- */
+* rot13 -  a function that encodes a string using rot13.
+* @s: An input string to encode using rot13
+* Return: An encode string
+*/
 char *rot13(char *s)
 {
 int i = 0;
