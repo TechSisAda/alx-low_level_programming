62ff423f3f441cebf5c1dbe244e7e5fbbaca7ff5
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2022-09-26 16:27:23 +0100

    even more pointers

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,13 @@
 char *_strchr(char *s, char c)
 {
 while (*s)
-	{
-		if (*s != c)
-			s++;
-		else
-			return (s);
-	}
-	if (c == '\0')
-		return (s);
-
-	return (NULL);
+{
+if (*s != c)
+s++;
+else
+return (s);
+}
+if (c == '\0')
+return (s);
+return (NULL);
 }
