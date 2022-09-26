99c4df855e3cebadbf66d8a678da0594ef9f5456
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2022-09-26 18:56:34 +0100

    prints chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_chessboard - function prints the chessboard
+ * @a: array input to print
+ * Return: always 0.
+ */
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+if (j == 7)
+{
+_putchar(a[i][j]);
+_putchar('\n');
+}
+else
+_putchar(a[i][j]);
+}
+}
+}
