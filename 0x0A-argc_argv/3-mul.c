01ffd9b743e8e914a005297f348ca9fde863d7ac
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2022-09-30 20:17:23 +0100

    argv and argc

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,25 +1,32 @@
-#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <main.h>
+#include "main.h"
+
 /**
- * main - multiplies two numbers
- * @argc: argument counter
- * @argv: argument vector
- * Return: 0 if it works, 
- * 1 if there is an ERROR
+ * main - Program that takes first two integer arguments and prints the product
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if not enough arguments passed in, 0 otherwise
  */
+
 int main(int argc, char *argv[])
 {
-int n, m, diff;
-if (argc != 3)
+int i, j;
+
+if (argc == 1 || argc == 2)
 {
 printf("Error\n");
 return (1);
 }
-n = atoi(argv[1]);
-m = atoi(argv[2]);
-diff = n * m;
-printf("%i\n", diff);
+else
+{
+j = 1;
+
+for (i = 1; i < 3; i++)
+j *= atoi(argv[i]);
+
+printf("%d\n", j);
+}
+
 return (0);
 }
